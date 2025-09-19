ed1e0d25bb405db97661be5138e4b0d463064d4b
Nethusha <nethu.rushane@gmail.com>
2025-09-19 16:42:19 +0530

    Working on the workouts.js

--- js/workouts.js
+++ js/workouts.js
@@ -22,4 +22,50 @@ const workouts = {
             "Band Front Raises: 3 sets of 12 reps"
         ]
     },
-    
+    'legs': {
+        'none': [
+            "Squats: 3 sets of 15 reps",
+            "Lunges: 3 sets of 12 reps per leg",
+            "Calf Raises: 3 sets of 20 reps",
+            "Glute Bridges: 3 sets of 15 reps",
+            "Wall Sit: 3 sets of 30 seconds"
+        ],
+        'dumbbells': [
+            "Goblet Squats: 3 sets of 12 reps",
+            "Dumbbell Lunges: 3 sets of 10 reps per leg",
+            "Deadlifts: 3 sets of 12 reps",
+            "Step-ups: 3 sets of 10 reps per leg",
+            "Calf Raises with Dumbbells: 3 sets of 15 reps"
+        ],
+        'resistance-band': [
+            "Band Squats: 3 sets of 15 reps",
+            "Band Side Steps: 3 sets of 12 reps per side",
+            "Band Kickbacks: 3 sets of 15 reps per leg",
+            "Band Leg Press: 3 sets of 12 reps",
+            "Band Calf Raises: 3 sets of 20 reps"
+        ]
+    },
+    'full-body': {
+        'none': [
+            "Burpees: 3 sets of 10 reps",
+            "Mountain Climbers: 3 sets of 20 reps",
+            "Plank: 3 sets of 30 seconds",
+            "Jumping Jacks: 3 sets of 30 seconds",
+            "Bodyweight Squat to Push-up: 3 sets of 10 reps"
+        ],
+        'dumbbells': [
+            "Dumbbell Thrusters: 3 sets of 12 reps",
+            "Renegade Rows: 3 sets of 10 reps per side",
+            "Dumbbell Deadlift to Press: 3 sets of 12 reps",
+            "Dumbbell Clean and Press: 3 sets of 10 reps",
+            "Dumbbell Snatch: 3 sets of 8 reps per side"
+        ],
+        'resistance-band': [
+            "Band Thrusters: 3 sets of 12 reps",
+            "Band Rows: 3 sets of 15 reps",
+            "Band Squat to Press: 3 sets of 12 reps",
+            "Band Chest Press: 3 sets of 12 reps",
+            "Band Deadlifts: 3 sets of 15 reps"
+        ]
+    }
+};
