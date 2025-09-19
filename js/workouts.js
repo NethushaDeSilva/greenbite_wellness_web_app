e29f9a91023393e3af2c2af682e20e4b07986fc4
Nethusha <nethu.rushane@gmail.com>
2025-09-19 16:39:54 +0530

    Started working on the workouts.js

---
+++ js/workouts.js
@@ -0,0 +1,25 @@
+const workouts = {
+    'arms': {
+        'none': [
+            "Push-ups: 3 sets of 12 reps",
+            "Tricep Dips (chair): 3 sets of 10 reps",
+            "Arm Circles: 3 sets of 30 seconds",
+            "Plank to Push-up: 3 sets of 10 reps",
+            "Handstand Hold: 3 sets of 20 seconds"
+        ],
+        'dumbbells': [
+            "Bicep Curls: 3 sets of 12 reps",
+            "Tricep Extensions: 3 sets of 10 reps",
+            "Shoulder Press: 3 sets of 12 reps",
+            "Hammer Curls: 3 sets of 10 reps",
+            "Lateral Raises: 3 sets of 12 reps"
+        ],
+        'resistance-band': [
+            "Band Bicep Curls: 3 sets of 15 reps",
+            "Band Tricep Pushdowns: 3 sets of 12 reps",
+            "Band Pull-Aparts: 3 sets of 15 reps",
+            "Band Overhead Press: 3 sets of 12 reps",
+            "Band Front Raises: 3 sets of 12 reps"
+        ]
+    },
+    
