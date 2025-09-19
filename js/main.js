701d306d8eac5a7e132210e55cc5a7abc441a132
Nethusha <nethu.rushane@gmail.com>
2025-09-19 15:49:42 +0530

    Made some minor chnages to the code due to errors

--- js/main.js
+++ js/main.js
@@ -1,6 +1,7 @@
 // Reusable save to localStorage
 function saveToLocal(key, value) {
     let data = JSON.parse(localStorage.getItem(key)) || [];
+    if (!Array.isArray(data)) data = [data];
     data.push(value);
     localStorage.setItem(key, JSON.stringify(data));
 }
@@ -14,7 +15,7 @@ document.querySelector('.hamburger').addEventListener('click', () => {
 document.getElementById('newsletter-form').addEventListener('submit', (e) => {
     e.preventDefault();
     const email = document.getElementById('newsletter-email').value;
-    if (/^\S+@\S+\.\S+$/.test(email) {
+    if (/^\S+@\S+\.\S+$/.test(email)){
         saveToLocal('subscribers', email);
         alert('Subscribed successfully!');
     } else {
